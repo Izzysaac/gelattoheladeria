faeecfd103812c22691c1e354f749780330cef19
Izzysaac <icolmenaresga@gmail.com>
2026-01-29 16:21:51 -0500

    feat: Enhance menu item display and add SVG icons for controls

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -6,7 +6,6 @@ export function formatFromThousands(value) {
   return Math.floor(num / 1000).toString();
 }
 
-
 export function cloudinaryUrl(id) {
   return `https://res.cloudinary.com/dc8vxeapd/image/upload/w_400,q_auto,f_auto/${id}.jpg`;
 }
