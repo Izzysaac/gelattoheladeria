d5addf62e5f34f0bb69594d4756060e8cec50ab5
Izzysaac <icolmenaresga@gmail.com>
2026-02-12 15:02:51 -0500

    remove public js

--- public/scripts/init.js
+++
@@ -1,9 +0,0 @@
-import { cargarState } from "./actions.js";
-import { bindEventos } from "./events.js";
-import { renderTodo } from "./render.js";
-
-export const init = () => {
-  cargarState();
-  bindEventos();
-  renderTodo();
-};
--- public/scripts/state.js
+++
@@ -1,5 +0,0 @@
-export const state = {
-  items: {},
-  tipoEntrega: "domicilio", // "domicilio" | "recoger"
-  direccion: "",
-};
