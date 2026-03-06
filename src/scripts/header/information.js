3b3ced9afb700f54f3cf325e1fcf7aeb41564c8b
Izzysaac <icolmenaresga@gmail.com>
2026-03-06 08:41:16 -0500

    text uppercase

--- src/scripts/header/information.js
+++ src/scripts/header/information.js
@@ -4,7 +4,7 @@ const info = document.getElementById("informacion");
 const infoBtn = document.getElementById("infoBtn");
 const closeInfoBtn = document.getElementById("closeInfoBtn");
 
-infoBtn.addEventListener("click", () => openModal("info", info));
+infoBtn?.addEventListener("click", () => openModal("info", info));
 info.addEventListener("click", (e) => {if (e.target  === info) manualClose()});
 closeInfoBtn.addEventListener("click", () => manualClose());
 
